ebb88654a987bc083b15a5f9dfc6ea33fe45f630
majdsafira <majdsafira@gmail.com>
2022-03-31 01:18:17 +0300

    main

--- JS-TASK/js-task-30-3-2022/task1/task1.js
+++ JS-TASK/js-task-30-3-2022/task1/task1.js
@@ -1,38 +1,68 @@
-var fname = document.getElementById('fname').value;
-var lname = document.getElementById('lname').value;
-var bd = document.getElementById('bd').value;
-var email = document.getElementById('em').value;
-var confirm = document.getElementById('con').value;
-var password = document.getElementById('pass').value;
-var confirmPass = document.getElementById('passCon').value;
+
 
 // check var
-var ch = /^[a-zA-Z]/;
-var pass = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var ch = /^[a-zA-Z]/g;
+var ee = /[@ \.]/g;
+var pas = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 
 function reg() {
-    checkName();
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var bd = document.getElementById('bd').value;
+    var email = document.getElementById('em').value;
+    var confir = document.getElementById('con').value;
+    var password = document.getElementById('pass').value;
+    var confirmPass = document.getElementById('passCon').value;
+
+
+    checkName(fname, lname);
+    checkEmail(email);
+    checkEmailCon(confir);
+    checkPass(password);
+    checkPassCon(confirmPass);
 
 }
 
-function checkName() {
-    if (fname.match(ch) && lname.match(ch)) {
-        return true;
+function checkName(fname, lname) {
+    if (fname.match(ch) && lname.match(ch)) { 
     }
     else {
-        return "please check your first name";
+        window.alert("please check your first name");
     }
 }
 
-function checkEmail() {
-    if (email.match(pass)) {
-        window.alert("error")
-        return true;
-    
+function checkEmail(email) {
+    if (email.match(ee)) {
+        window.alert("i am work");
     }
     else {
+        window.alert("please check your email ");
     
-         return false;
-    
+    }    
+}
+
+function checkPass(password) {
+    if(password.match(pas)) {
+    }
+    else {
+        window.alert("please check your password");
+    }
+}
+
+
+function checkEmailCon(confir) {
+    if (email = confir) {
+    }
+    else {
+        window.alert("please enter the same email to confirm it");
+    }
+}
+
+
+function checkPassCon(confirmPass) {
+    if (password = confirmPass) {
+    }
+    else {
+        window.alert("please check your password");
     }
 }
